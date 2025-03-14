b75c4f7fbcd75bffcee238565730fae446036fc6
faiz adenle <adenle.sadikoufaiz@gmail.com>
2025-03-14 14:48:09 +0100

    Initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,41 @@
-
 import './App.css'
 import Wrapper from './components/Wrapper'
-import Plot from 'react-plotly.js';
+import Plot from 'react-plotly.js'
 
-function App() {
+const App = () => {
+  // Données pour les hommes et les femmes
+  const maleData: any = {
+    x: [1, 2, 3],
+    y: [2, 6, 3],
+    type: 'scatter',
+    mode: 'lines+markers',
+    name: 'Male', // Nom dans la légende
+    marker: { color: 'blue' },
+  }
 
+  const femaleData: any = {
+    x: [1, 2, 3],
+    y: [1, 4, 2],
+    type: 'scatter',
+    mode: 'lines+markers',
+    name: 'Female', // Nom dans la légende
+    marker: { color: 'pink' },
+  }
 
   return (
-    <Wrapper >
- <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: {text: 'A Fancy Plot'}} }
-      />
+    <Wrapper>
+      <div className="flex flex-col">
+        <Plot
+          data={[maleData, femaleData]} // Affiche les deux traces
+          config={{
+            responsive: true,
+            displayModeBar: true,
+            displaylogo: false,
+            staticPlot: false,
+          }}
+          layout={{  title: { text: 'A Fancy Plot' } }}
+        />
+      </div>
     </Wrapper>
   )
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 
 const Menu = () => {
   return (
-    <div className="navbar bg-base-100 flex justify-between border-b border-base-200 px-5 md:px-[5%] ">
+    <div className="navbar bg-base-100 flex justify-between border-b border-base-200 px-5 md:px-[5%]  shadow">
       <div className="">
         <a className="text-xl font-bold">LoveMyPet PSID</a>
       </div>
       <div className="">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Acceuil</a></li>
-          <li><a>Tableau de bord</a></li>
-          <li><a>ML</a></li>
+        <ul className="menu menu-horizontal space-x-2">
+          <li><a className="btn btn-primary">Acceuil</a></li>
+          <li><a className="btn btn-primary">Tableau de bord</a></li>
+          <li><a className="btn btn-primary">ML</a></li>
         </ul>
       </div>
     </div>
